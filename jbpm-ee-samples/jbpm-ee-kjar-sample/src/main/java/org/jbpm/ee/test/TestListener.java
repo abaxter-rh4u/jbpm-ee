67f4e2ca827bd0bb9a1798ea0777a4c6622fbdcc
Adam Baxter <abaxter@redhat.com>
2014-01-08 12:57:20 -0500

    Added WIH and Listener to kmodule.xml

---
+++ jbpm-ee-samples/jbpm-ee-kjar-sample/src/main/java/org/jbpm/ee/test/TestListener.java
@@ -0,0 +1,65 @@
+package org.jbpm.ee.test;
+
+import org.kie.api.event.process.ProcessCompletedEvent;
+import org.kie.api.event.process.ProcessEventListener;
+import org.kie.api.event.process.ProcessNodeLeftEvent;
+import org.kie.api.event.process.ProcessNodeTriggeredEvent;
+import org.kie.api.event.process.ProcessStartedEvent;
+import org.kie.api.event.process.ProcessVariableChangedEvent;
+import org.slf4j.LoggerFactory;
+
+public class TestListener implements ProcessEventListener {
+
+	private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(ProcessEventListener.class);
+	
+	public void beforeProcessStarted(ProcessStartedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterProcessStarted(ProcessStartedEvent event) {
+		// TODO Auto-generated method stub
+		LOG.info("Process started: " + event.getProcessInstance().getId());
+	}
+
+	public void beforeProcessCompleted(ProcessCompletedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterProcessCompleted(ProcessCompletedEvent event) {
+		// TODO Auto-generated method stub
+		LOG.info("Process completed: " + event.getProcessInstance().getId());
+	}
+
+	public void beforeNodeTriggered(ProcessNodeTriggeredEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterNodeTriggered(ProcessNodeTriggeredEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeNodeLeft(ProcessNodeLeftEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterNodeLeft(ProcessNodeLeftEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeVariableChanged(ProcessVariableChangedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterVariableChanged(ProcessVariableChangedEvent event) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
